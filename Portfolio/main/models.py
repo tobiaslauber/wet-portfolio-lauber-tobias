61ad7e5b8181a585ae21b600576a725be38f8556
Tobi <tobias.lauber1@students.fhnw.ch>
2025-12-12 00:12:18 +0100

    sqlite und many to many

--- Portfolio/main/models.py
+++ Portfolio/main/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Skill(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    gitlab_url = models.URLField()
+
+    # Many-to-Many Beziehung
+    skills = models.ManyToManyField(Skill, related_name="projects")
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
--- Portfolio/main/views.py
+++ Portfolio/main/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from .models import Project, Skill
+
 
 def home(request):
-    return render(request, "main/home.html")
+    # Alle Projekte und Skills aus der Datenbank holen
+    projects = Project.objects.all()
+    skills = Skill.objects.all()
+
+    # An das Template schicken
+    return render(request, "main/home.html", {
+        "projects": projects,
+        "skills": skills,
+    })
+
 
 def cv(request):
     return render(request, "main/cv.html")
-
-
